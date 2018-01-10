5ff91c2deead5447fc58c8bf0d3d07fe80699190
jyano <junpei1310@gmail.com>
2018-01-11 00:12:09 +0900

    別のtomcatでデプロイできるように修正

--- src/main/java/com/istz/DokkanbattleApplication.java
+++ src/main/java/com/istz/DokkanbattleApplication.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class DokkanbattleApplication implements CommandLineRunner {
+public class DokkanbattleApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DokkanbattleApplication.class, args);
@@ -17,8 +19,13 @@ public class DokkanbattleApplication implements CommandLineRunner {
 	JdbcTemplate jdbc;
 
     // アプリ起動時に実行される。
-    @Override public void run(String... args) throws Exception {
+    @Override
+    public void run(String... args) throws Exception {
         jdbc.execute("create table task (id serial, name varchar(255))");
         jdbc.update("insert into task (name) values (?)", "First Task.");
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DokkanbattleApplication.class);
     }
 }
